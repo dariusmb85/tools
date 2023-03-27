10861ee897f217930c834c8da188f1f1579393ae
Howard Lander <howard@renci.org>
2023-03-27 16:52:11 -0400

    much better version with comments and using a file for the bounding box instead of 4 different parameters.

--- clipToBoundingBox.py
+++ clipToBoundingBox.py
@@ -1,50 +1,51 @@
 #!/usr/bin/env python3
+# This code reads in an RTI synthetic population CSV file and outputs only the entries that
+# are within the specified bounding box.
 import csv
 import argparse, sys
 
 parser=argparse.ArgumentParser()
 
-parser.add_argument("--inputFile", help="The file to trim")
+parser.add_argument("--inputFile", help="The file to clip")
 parser.add_argument("--outputFile", help="The file to output")
-parser.add_argument("--north", help="The north point of the bounding box")
-parser.add_argument("--east", help="The east point of the bounding box")
-parser.add_argument("--south", help="The south point of the bounding box")
-parser.add_argument("--west", help="The west point of the bounding box")
+parser.add_argument("--boxFile", help="A file containing the bounding box as North, West, South, East")
 
 args=parser.parse_args()
-print(args)
-print(args.north)
-north = float(args.north)
-south = float(args.south)
-east = float(args.east)
-west = float(args.west)
 
+# Open the box file.  
+for line in  open(args.boxFile):
+   li=line.strip()
+   # skip any comment lines
+   if not li.startswith("#"):
+      # The first non comment line is the bounding box
+      # The bounding coordinates are "," separated
+      coordList = li.split(",")
+      north = float(coordList[0])
+      west = float(coordList[1])
+      south = float(coordList[2])
+      east = float(coordList[3])
+
+# Grab the output file name from the args and open it for writing
+output = args.outputFile
+outputFile = open(output, 'w')
+filewriter = csv.writer(outputFile)
+
+# Open the input file
 with open(args.inputFile, newline='') as csvfile:
     filereader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    next(filereader)
-    counter = 0
+
+    # grab the header and add it to the output file
+    header = next(filereader)
+    filewriter.writerow(header)
+
+    # for each row in the file, perform the bounding box test.
     for row in filereader:
-       #print('LON ' + row[16])
-       #print('LAT ' + row[17])
+
+       # I am unhappy that these fields have to be hard coded.  Either I should find a way to recode
+       # this (maybe using pandas) or we should pass the LAT LON indices in as parameters.
        lon = float(row[16])
        lat = float(row[17])
-       print(str(lat) + " " + str(lon) + " " + str(north)+ " " + str(south)+ " " + str(east) + " " + str(west))
-#      if lat < north:
-#         print ("lat < north")
-          
-#      if lat > south:
-#         print ("lat > south")
-
-#      if lon < east:
-#         print ("lon < east")
-
-#      if lon > west:
-#         print ("lon > west")
-          
        if lat < north and lat > south and lon > west and lon < east:
-          print ("row found")
-          print (row)
-       else:
-          counter = counter + 1
-         # if (counter == 100):
-         #    sys.exit(0)
+          filewriter.writerow(row)
+
+    outputFile.close()     
